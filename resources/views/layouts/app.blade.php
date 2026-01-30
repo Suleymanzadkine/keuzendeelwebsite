29c0177c4fda092a4b2eecf1b969327883d84c4e
Suleymanzadkine <9024417@student.zadkine.nl>
2026-01-30 11:13:35 +0100

    drop downs fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Alpine.js voor dropdowns en interactieve componenten -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="font-sans antialiased">
